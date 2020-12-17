4eebf57968a3faa4feb636c6921036624139caa7
Your Name <aboode114g2@gmail.com>
2020-12-17 17:39:08 +0200

    s=js

---
+++ js/script.js
@@ -0,0 +1,67 @@
+
+var ham3 = document.querySelector('.ham3');
+var ul_list3 = document.querySelector('.ul-list3');
+
+
+ham3.addEventListener('click' ,() =>{
+  ul_list3.classList.toggle('show3')
+});
+
+/*
+document.querySelector('.hamburger').addEventListener('click',() => document.querySelector('.ul-list2').classList.toggle('show'));
+/*
+ham3.addEventListener('click' ,() =>{
+  if(ham3.style.backgroundColor =='blue'){
+   ham3.style.backgroundColor='red';
+  }
+  else{
+    ham3.style.backgroundColor='blue';
+}
+});
+
+
+console.log('2');
+
+*/
+
+/*
+var ham = document.querySelector('#ham');
+var listmenu = document.querySelector('#listmenu');
+
+ham.addEventListener('click',function(fucmenuopen){
+
+});
+
+
+ham.addEventListener('click' ,()  =>{
+	listmenu.toggle.classList('.show')
+
+});
+
+function fucmenuopen(e){
+	listmenu.toggle.classList('.show')
+}
+-----------------------------------------
+ham.addEventListener('click' ,()  =>{
+	if(ham.style.color===red;){
+		ham.style.color='';
+	else{ham.style.color='red';
+	};
+}
+	
+
+});
+
+
+---------------------------------------------------
+#item-1 #item-2 #item-3
+#item-1-content  #item-2-content  #item-3-content
+
+forEach(item => item.addEventListener('click', funcnamae())
+
+
+
+
+
+
+*/
